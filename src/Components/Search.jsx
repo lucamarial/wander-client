b4fba2af31c022f24c7091e13a2560dc4541bc52
luca <l.lobacher@web.de>
2019-11-19 17:51:09 +0100

    Modifies search functionality

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,38 +1,67 @@
 import React, { Component } from "react"
 import { Menu, Input, Icon } from "semantic-ui-react"
 import { withRouter } from "react-router-dom"
-import { searchTrail } from "../Modules/trailsData"
+import { searchTrail, getTrails } from "../Modules/trailsData"
 
 class Search extends Component {
   state = {
+    trails: [],
     query: "",
     errorMessage: "",
     searchResults: []
   }
 
+  async componentDidMount() {
+    let response = await getTrails()
+    if (response.error_message) {
+      this.setState({
+        errorMessage: response.error_message
+      })
+    } else {
+      this.setState({
+        trails: response
+      })
+    }
+  }
+
   inputChangeHandler = e => {
+    let currentList = []
+    let newList = []
+
+    if(e.target.value !== '') {
+      debugger
+      currentList = this.props.trails
+      newList = currentList.filter(trail => {
+        const lc = trail.toString().toLowerCase()
+        const filter = e.target.value.toLowerCase()
+        return lc.includes(filter)
+      })
+    } else {
+      newList = this.props.trails
+    }
+
     this.setState({
-      [e.target.name]: e.target.value
+      searchResults: newList
     })
   }
 
-  getSearchResults = async () => {
-    const { query } = this.state
-    if (this.state.query.length < 3) {
-      this.setState({
-        errorMessage: 'Please input more than two characters.'
-      })
-    } else {
-      let response = await searchTrail(query)
-      if (response.error_message) {
-        this.setState({
-          errorMessage: response.error_message
-        })
-      } else {
-        this.props.history.push("/search", { searchResults: response })
-      }
-    }
-  }
+  // getSearchResults = async () => {
+  //   const { query } = this.state
+  //   if (this.state.query.length < 3) {
+  //     this.setState({
+  //       errorMessage: 'Please input more than two characters.'
+  //     })
+  //   } else {
+  //     let response = await searchTrail(query)
+  //     if (response.error_message) {
+  //       this.setState({
+  //         errorMessage: response.error_message
+  //       })
+  //     } else {
+  //       this.props.history.push("/search", { searchResults: response })
+  //     }
+  //   }
+  // }
 
   render() {
     let search, errorMessage
